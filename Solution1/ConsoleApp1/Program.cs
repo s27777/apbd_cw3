d081a63958ddc6c31318edec0d9ce37ca1dde3d3
s27777 <s27777@pjwstk.edu.pl>
2025-03-20 19:15:32 +0100

    project created"

---
+++ Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("initial");
+            
+        }
+    }
+}
